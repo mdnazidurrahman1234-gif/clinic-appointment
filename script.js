d95cb80645da8d5bafc8efcada358aec136624d4
mdnazidurrahman1234-gif <mdnazidurrahman1234@gmail.com>
2026-07-17 20:00:58 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,150 +1,59 @@
-//==============================
-// Save Appointment
-//==============================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-const form=document.getElementById("appointmentForm");
+import {
+getDatabase,
+ref,
+push
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-if(form){
+const firebaseConfig = {
+  apiKey: "AIzaSyD6wKkpZ9KmRi1_nPmBrvmAk-v5YSLRczo",
+  authDomain: "clinic-appointment-72505.firebaseapp.com",
+  databaseURL: "https://clinic-appointment-72505-default-rtdb.firebaseio.com",
+  projectId: "clinic-appointment-72505",
+  storageBucket: "clinic-appointment-72505.firebasestorage.app",
+  messagingSenderId: "969154219142",
+  appId: "1:969154219142:web:5f40ff23ae2d7cafb95507"
+};
 
-form.addEventListener("submit",function(e){
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+document.getElementById("appointmentForm").addEventListener("submit",function(e){
 
 e.preventDefault();
 
 const appointment={
-id:Date.now(),
+
 name:document.getElementById("name").value,
+
 phone:document.getElementById("phone").value,
+
 doctor:document.getElementById("doctor").value,
+
 date:document.getElementById("date").value,
-time:document.getElementById("time").value
+
+time:document.getElementById("time").value,
+
+createdAt:new Date().toISOString()
+
 };
 
-let appointments=JSON.parse(localStorage.getItem("appointments"))||[];
+push(ref(db,"appointments"),appointment)
 
-appointments.push(appointment);
-
-localStorage.setItem("appointments",JSON.stringify(appointments));
+.then(()=>{
 
 alert("Appointment Saved Successfully");
 
-form.reset();
+document.getElementById("appointmentForm").reset();
+
+})
+
+.catch((error)=>{
+
+alert(error.message);
 
 });
 
-}
-
-
-
-//==============================
-// Show Appointment List
-//==============================
-
-function loadAppointments(){
-
-const table=document.getElementById("appointmentTable");
-
-if(!table) return;
-
-const appointments=JSON.parse(localStorage.getItem("appointments"))||[];
-
-table.innerHTML="";
-
-appointments.forEach((item,index)=>{
-
-table.innerHTML+=`
-
-<tr>
-
-<td>${item.name}</td>
-
-<td>${item.phone}</td>
-
-<td>${item.doctor}</td>
-
-<td>${item.date}</td>
-
-<td>${item.time}</td>
-
-<td>
-
-<button class="edit" onclick="editAppointment(${index})">Edit</button>
-
-<button class="delete" onclick="deleteAppointment(${index})">Delete</button>
-
-</td>
-
-</tr>
-
-`;
-
 });
-
-}
-
-loadAppointments();
-
-
-
-//==============================
-// Delete
-//==============================
-
-function deleteAppointment(index){
-
-let appointments=JSON.parse(localStorage.getItem("appointments"))||[];
-
-if(confirm("Delete this appointment?")){
-
-appointments.splice(index,1);
-
-localStorage.setItem("appointments",JSON.stringify(appointments));
-
-loadAppointments();
-
-}
-
-}
-
-
-
-//==============================
-// Edit
-//==============================
-
-function editAppointment(index){
-
-let appointments=JSON.parse(localStorage.getItem("appointments"))||[];
-
-let newName=prompt("Patient Name",appointments[index].name);
-
-if(newName==null) return;
-
-appointments[index].name=newName;
-
-localStorage.setItem("appointments",JSON.stringify(appointments));
-
-loadAppointments();
-
-}
-
-
-
-//==============================
-// Search
-//==============================
-
-function searchAppointment(){
-
-let input=document.getElementById("search").value.toLowerCase();
-
-let rows=document.querySelectorAll("#appointmentTable tr");
-
-rows.forEach(function(row){
-
-let text=row.innerText.toLowerCase();
-
-row.style.display=text.includes(input)?"":"none";
-
-});
-
-}
